14e774f6fdcd394bcc3d17c311ff7219bc7b056f
Tom Davidson <tom@davidson.me.uk>
2014-11-19 22:09:14 +0000

    Creates user and gets lights state.

--- huepi.py
+++ huepi.py
@@ -4,6 +4,7 @@ BRIDGE_IP = '192.168.10.14'
 BRIDGE_USER = 'huepihuepi'
 
 def createUser():
+    # http://www.developers.meethue.com/documentation/configuration-api#71_create_user
     global BRIDGE_IP, BRIDGE_USER
     data = {
         "devicetype":"hue#pi",
@@ -14,16 +15,17 @@ def createUser():
         False,
         data
     )
-    print r.json()
+    return r.json()
 
 
 def getLights():
+    # http://www.developers.meethue.com/documentation/lights-api#11_get_all_lights
     global BRIDGE_IP, BRIDGE_USER
     r = requests.get('http://%s/api/%s/lights' % (BRIDGE_IP, BRIDGE_USER))
     return r.json()
 
 def main():
-    print createUser()
+    print getLights()
 
 if __name__ == '__main__':
     main()
