23c4ef5e6fb932cec044480edd2f49648054408a
Tom Davidson <tom@davidson.me.uk>
2014-11-19 21:10:17 +0000

    Application skeleton.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ huePi
 =====
 
 Application for the Raspberry Pi to control the Philips Hue lights.
+
+
+Notes
+=====
+http://roboticape.wordpress.com/2013/10/08/magic-lights-controlling-hue-with-a-raspberry-pi/
+http://www.everyhue.com/vanilla/discussion/261/controlling-philips-hue-with-raspberry-pi-python-and-physical-switches
+https://github.com/kitekid250/RaspberryPi/blob/master/Libraries/ouimeaux-develop/setup.py
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+here = lambda *a: os.path.join(os.path.dirname(__file__), *a)
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = open(here('README.md')).read()
+requirements = [x.strip() for x in open(here('requirements.txt')).readlines()]
+setup(
+    name='huepi',
+    version='0.1.0',
+    description='Application for the Raspberry Pi to control the Philips Hue lights.',
+    long_description=readme,
+    author='Tom Davidson',
+    author_email='tom@davidson.me.uk',
+    url='https://github.com/Tom-Davidson/huePi',
+    packages=[
+        'hue',
+    ],
+    package_dir={'hue': 'hue'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='huepi',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Home Automation',
+    ],
+    test_suite='tests',
+)
